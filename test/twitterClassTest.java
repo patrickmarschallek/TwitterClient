4ba3323e6bafaff364f9bbd3b2fdd5bba041a53f
Patrick Marschallek <patrick.marschallek@hotmgooglemail.com>
2011-11-12 12:16:22 +0100

    add Testdirectory with testclass

---
+++ test/twitterClassTest.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class twitterClassTest {
+
+	@Test
+	public final void testTwitterClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testTwitterClassStringString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testFollow() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testRetweet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testShutdown() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
